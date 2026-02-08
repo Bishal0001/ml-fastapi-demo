14035d57e4ccb6adf44d3d0c46bc561ad2732b73
Bishal <roybishal422@gmail.com>
2026-02-08 15:08:24 +0530

    test auto deploy

--- demoAPP.py
+++ demoAPP.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 
 @app.get("/predict")
-def predict_model(age: int, sex:str):
-    if age <= 15 and sex == 'F':
+def predict_model(age: int, sex:str, income:int):
+    if age <= 15 and sex == 'F' and income>10000:
         return {'survived':1}
     return {'survived':0}
 
